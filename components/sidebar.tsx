3e48cc44861436be504fa40cb72ea2b7c546e1d6
pinnng29 <apingspeedholic@gmail.com>
2024-03-16 09:50:39 +0700

    Sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,17 +1,66 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import { cn } from "@/lib/utils";
+import { SidebarItem } from "@/components/sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 type Props = {
   className?: string;
 };
 
-
 export const Sidebar = ({ className }: Props) => {
   return (
-    <div className={cn(
-      "flex bg-blue-500 h-full lg:w-[256px] lg:fixed left-0 px-4 border-r flex-col",
-      className
-      )}>
-      Sidebar
+    <div
+      className={cn(
+        "flex h-full lg:w-[256px] lg:fixed left-0 px-4 border-r flex-col",
+        className
+      )}
+    >
+      <Link href={"/learn"}>
+        <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
+          <Image
+            src={"/mascot.svg"}
+            alt="Mascot"
+            width={40}
+            height={40}
+          />
+          <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
+            Lingo
+          </h1>
+        </div>
+      </Link>
+      <div className="flex flex-col gap-y-2 flex-1">
+        <SidebarItem
+          label="Learn"
+          href="/learn"
+          iconSrc="/learn.svg"
+        />
+        <SidebarItem
+          label="Leaderboard"
+          href="/leaderboard"
+          iconSrc="/leaderboard.svg"
+        />
+        <SidebarItem
+          label="Quest"
+          href="/quest"
+          iconSrc="/quests.svg"
+        />
+        <SidebarItem
+          label="Shop"
+          href="/shop"
+          iconSrc="/shop.svg"
+        />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+          <Loader className="h-8 w-8 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <UserButton afterSignOutUrl="/"/>
+        </ClerkLoaded>
+      </div>
     </div>
   );
 };
