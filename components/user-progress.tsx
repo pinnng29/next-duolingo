51849b76826f874e113ecbdc20de43081c25ab96
pinnng29 <apingspeedholic@gmail.com>
2024-03-16 13:41:42 +0700

    Seed Script

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+
 import { InfinityIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import { courses } from "@/db/schema";
 
 type Props = {
-  activeCourse: { imageSrc: string; title: string }; // TODO: Replace with db
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
