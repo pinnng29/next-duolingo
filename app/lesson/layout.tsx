c9c64292d9fc5e89abfdd4e25f74b179f6e14f30
pinnng29 <apingspeedholic@gmail.com>
2024-03-18 04:56:24 +0700

    Challenge Cards

--- app/lesson/layout.tsx
+++ app/lesson/layout.tsx
@@ -2,12 +2,12 @@ type Props = {
   children: React.ReactNode;
 };
 
-export default function LessonLayout({ children }: Props) {
+const LessonLayout = ({ children }: Props) => {
   return (
     <div className="flex flex-col h-full">
-      <div className="flex flex-col h-full w-full">
-        {children}
-      </div>
+      <div className="flex flex-col h-full w-full">{children}</div>
     </div>
   );
 };
+
+export default LessonLayout;
